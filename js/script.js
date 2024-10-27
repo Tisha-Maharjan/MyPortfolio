450718a64a54e244c238d55bddbce06e2cb098cc
tiisham <tiishamaharjan@gmail.com>
2024-10-27 08:27:10 +0545

    first commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+"use strict";
+
+// Typing effect
+document.addEventListener("DOMContentLoaded", function () {
+  const typedTextSpan = document.querySelector(".typed-text");
+  const cursorSpan = document.querySelector(".cursor");
+
+  const textArray = ["Web Designer", "Web Developer", "Frontend Developer"];
+  const typingDelay = 100;
+  const erasingDelay = 100;
+  const newTextDelay = 2000;
+  let textArrayIndex = 0;
+  let charIndex = 0;
+
+  function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+      if (!cursorSpan.classList.contains("typing"))
+        cursorSpan.classList.add("typing");
+      typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(type, typingDelay);
+    } else {
+      cursorSpan.classList.remove("typing");
+      setTimeout(erase, newTextDelay);
+    }
+  }
+
+  function erase() {
+    if (charIndex > 0) {
+      if (!cursorSpan.classList.contains("typing"))
+        cursorSpan.classList.add("typing");
+      typedTextSpan.textContent = textArray[textArrayIndex].substring(
+        0,
+        charIndex - 1
+      );
+      charIndex--;
+      setTimeout(erase, erasingDelay);
+    } else {
+      cursorSpan.classList.remove("typing");
+      textArrayIndex++;
+      if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+      setTimeout(type, typingDelay + 1100);
+    }
+  }
+
+  setTimeout(type, newTextDelay + 250);
+});
+
+// Sticky header
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector("header");
+  const aboutSection = document.getElementById("about");
+
+  function toggleStickyHeader() {
+    if (window.scrollY > aboutSection.offsetTop - header.offsetHeight) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  }
+
+  window.addEventListener("scroll", toggleStickyHeader);
+});
